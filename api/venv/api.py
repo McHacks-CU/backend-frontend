6d06bd1571c9a4a337bd97d7d03b5e2b5305c9fa
colin_mccormack <colin.william.mccormack@gmail.com>
2023-01-28 15:01:12 -0500

    add connection between backend-frontend

--- api/venv/api.py
+++ api/venv/api.py
@@ -3,10 +3,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return {'hello world'}
-
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}

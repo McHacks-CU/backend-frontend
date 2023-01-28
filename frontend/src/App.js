f5a60fc80a4c99da609eba0c4c6109d6591612a3
Kevin Wu <kevwuhoo15@gmail.com>
2023-01-28 15:17:43 -0500

    routing not working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
-import LandingPage from './components/FirstPage/LandingPage';
+// import './App.css';
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Layout from './pages/Layout';
+// import LandingPage from './pages/LandingPage';
+// import GetStarted from './pages/GetStarted';
 
-function App() {
-  return (
-    <LandingPage />
-  );
-}
-
-export default App;
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Layout />}>
+//           <Route index element ={<LandingPage />} />
+//           <Route path="getstarted" element={<GetStarted />} />
+//         </Route>
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+// export default App;
--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -2,12 +2,13 @@
 import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Outlet, Link } from "react-router-dom";
 
 const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
+//   { name: 'Get Started', href: '#' },
+//   { name: 'About', href: '#' },
+//   { name: 'Marketplace', href: '#' },
+//   { name: 'Company', href: '#' },
 ]
 
 const LandingPage = () => {
@@ -36,7 +37,7 @@ const LandingPage = () => {
               gradientUnits="userSpaceOnUse"
             >
               <stop stopColor="#9089FC" />
-              <stop offset={1} stopColor="#FF80B5" />
+              <stop offset={1} stopColor="#8091ff" />
             </linearGradient>
           </defs>
         </svg>
@@ -66,11 +67,11 @@ const LandingPage = () => {
               </a>
             ))}
           </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          {/* <div className="hidden lg:flex lg:flex-1 lg:justify-end">
             <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
               Log in <span aria-hidden="true">&rarr;</span>
             </a>
-          </div>
+          </div> */}
         </nav>
         <Dialog as="div" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
           <Dialog.Panel focus="true" className="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden">
@@ -101,14 +102,14 @@ const LandingPage = () => {
                     </a>
                   ))}
                 </div>
-                <div className="py-6">
+                {/* <div className="py-6">
                   <a
                     href="#"
                     className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10"
                   >
                     Log in
                   </a>
-                </div>
+                </div> */}
               </div>
             </div>
           </Dialog.Panel>
@@ -118,31 +119,29 @@ const LandingPage = () => {
         <div className="relative px-6 lg:px-8">
           <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
             <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-              <div className="relative rounded-full py-1 px-3 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+              {/* <div className="relative rounded-full py-1 px-3 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                 Announcing our next round of funding.{' '}
                 <a href="#" className="font-semibold text-indigo-600">
                   <span className="absolute inset-0" aria-hidden="true" />
                   Read more <span aria-hidden="true">&rarr;</span>
                 </a>
-              </div>
+              </div> */}
             </div>
             <div className="text-center">
-              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                Data to enrich your online business
+              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-7xl">
+                Identify and Prevent SQL Security Issues
               </h1>
               <p className="mt-6 text-lg leading-8 text-gray-600">
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-                fugiat veniam occaecat fugiat aliqua.
+                Paste your SQL code into our textfield and see your code's vulnerabilities
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a
-                  href="#"
-                  className="rounded-md bg-indigo-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="rounded-md bg-indigo-400 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-indigo-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Get started
                 </a>
                 <a href="#" className="text-base font-semibold leading-7 text-gray-900">
-                  Learn more <span aria-hidden="true">→</span>
+                  How we do it <span aria-hidden="true">→</span>
                 </a>
               </div>
             </div>
@@ -168,13 +167,14 @@ const LandingPage = () => {
                   gradientUnits="userSpaceOnUse"
                 >
                   <stop stopColor="#9089FC" />
-                  <stop offset={1} stopColor="#FF80B5" />
+                  <stop offset={1} stopColor="#8091ff" />
                 </linearGradient>
               </defs>
             </svg>
           </div>
         </div>
       </main>
+      <Outlet />
     </div>
   );
 };

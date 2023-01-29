0259bc9b99335746c5b9a8f5879861a59da407df
Aekus Trehan <aekus.trehan@gmail.com>
2023-01-29 02:09:50 -0500

    Ouput from cohere ai is showing on website

--- api/env/api.py
+++ api/env/api.py
@@ -1,8 +1,11 @@
 import time
 from flask import Flask, request, render_template, jsonify
+import re
+
+# some_file.py
 import sys
 
-sys.path.append('../../ai')
+sys.path.insert(1, '../../ai')
 
 from model import classify
 
@@ -15,13 +18,40 @@ def form_example():
         try:
             body = request.get_json()
             text = body['text']
-            print(text)
+            # print(text)
         except:
             return jsonify(result='ERROR : 400, bad request')
         # check code
-        if (not classify(text)):
-            return jsonify(result="WARNING : Your code is vulnerable to SQL Injection!")
-        else:
-            return jsonify(result="Your code is safe!")
+        # print(text)
+        keywords = ["query", "execute", "WHERE", "executeQuery", "statement", "prepare", "conn", "connection"] # keywords identifying sql
+        sent_tokens = re.split(r'\n|;', text)
+        need_to_check = ''
+        for sentence in sent_tokens:
+            tokenized_sent = [word.lower() for word in re.split(' ', sentence)]
+            # print(tokenized_sent)
+            # if any item in the tokenized sentence is a keyword, append the original sentence
+            if any(keyw in tokenized_sent for keyw in keywords):
+                need_to_check += str(sentence)
+
+        print(need_to_check)
+        if (not need_to_check):
+            need_to_check = text
+
+        resulting_info = classify(text, need_to_check)
+                
+        # fileinF = []
+        # for sent in fileinE:
+        #     # tokenize and lowercase tokens of the sentence
+        #     tokenized_sent = [word.lower() for word in word_tokenize(sent)]
+        #     # if any item in the tokenized sentence is a keyword, append the original sentence
+        #     if any(keyw in tokenized_sent for keyw in keywords):
+        #         fileinF.append(sent)
+
+        return jsonify(result=resulting_info)
+
+        # if (not classify(text)):
+        #     return jsonify(result="WARNING : Your code is vulnerable to SQL Injection!")
+        # else:
+        #     return jsonify(result="Your code is safe!")
     # handle get request
     return jsonify(result='ERROR : 404, only POST req allowed or service not available')

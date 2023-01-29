a98092d670d171cb7fcfae4a9fa3bd49e0a97e27
Aekus Trehan <aekus.trehan@gmail.com>
2023-01-29 01:18:42 -0500

    Connecting models to work together to get output

--- ai/response.py
+++ ai/response.py
@@ -2,133 +2,185 @@ import os
 import cohere
 from dotenv import load_dotenv
 
-load_dotenv()
+def respond(code):
+    load_dotenv()
 
-co = cohere.Client(os.getenv("API_KEY"))
-
-response = co.generate(
-    model='command-medium-nightly',
+    co = cohere.Client(os.getenv("API_KEY"))
     prompt = '''This program generates tips on how to fix code that is vulnerable to sql injection.
 
-Prompt: "The following code is vulnerable to SQL injection. How can it be fixed?
-$username = $_POST['username'];
-$password = $_POST['password'];
+    Prompt: "The following code is vulnerable to SQL injection. How can it be fixed?
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-$query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
-$result = mysqli_query($connection, $query);
+    $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+    $result = mysqli_query($connection, $query);
+    "
+    Completion 1: Use prepared statements with parameterized queries to automatically escape any user input.
+    Completion 2: Use the mysqli_real_escape_string() function to escape any special characters in user input before using it in an SQL query.
+    Completion 3: Use an ORM (Object-Relational Mapping) library like Doctrine or Eloquent to handle the SQL queries, which provide a more secure way to handle user input.
+    --  
+    Prompt: "The following code is vulnerable to SQL injection. How can it be fixed?
+    const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
 
-if (mysqli_num_rows($result) > 0) {
-    // login successful
-} else {
-    // login failed
-}
-"
-Completion 1: Use prepared statements with parameterized queries to automatically escape any user input.
-Completion 2: Use the mysqli_real_escape_string() function to escape any special characters in user input before using it in an SQL query.
-Completion 3: Use an ORM (Object-Relational Mapping) library like Doctrine or Eloquent to handle the SQL queries, which provide a more secure way to handle user input.
---  
-Prompt: "The following code is vulnerable to SQL injection. How can it be fixed?
-const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
-
-client.query(query, (err, res) => {
-    if (err) {
-        console.log(err.stack)
-    } else {
-        if (res.rows.length > 0) {
-            // login successful
+    client.query(query, (err, res) => {
+        if (err) {
+            console.log(err.stack)
         } else {
-            // login failed
+            if (res.rows.length > 0) {
+                // login successful
+            } else {
+                // login failed
+            }
         }
+    });
+    "
+    Completion 1: Use prepared statements with parameterized queries instead of concatenating user input into the SQL query.
+    Completion 2: Use a library like pg-escape to escape any special characters in user input before using it in an SQL query.
+    Completion 3: Use an ORM (Object-Relational Mapping) library like Sequelize or TypeORM to handle the SQL queries, which provide a more secure way to handle user input.
+    --  
+    Prompt: "The following code is vulnerable to SQL injection. How can it be fixed?
+    string query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'";
+
+    SqlCommand command = new SqlCommand(query, connection);
+    SqlDataReader reader = command.ExecuteReader();
+
+    if (reader.HasRows)
+    {
+        // login successful
     }
-});
-"
-Completion 1: Use prepared statements with parameterized queries instead of concatenating user input into the SQL query.
-Completion 2: Use a library like pg-escape to escape any special characters in user input before using it in an SQL query.
-Completion 3: Use an ORM (Object-Relational Mapping) library like Sequelize or TypeORM to handle the SQL queries, which provide a more secure way to handle user input.
---  
-Prompt: "The following code is vulnerable to SQL injection. How can it be fixed?
-string query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'";
+    else
+    {
+        // login failed
+    }
+    "
+    Completion 1: Use prepared statements with parameterized queries to automatically escape any user input.
+    Completion 2: Use the SqlCommand.Parameters.AddWithValue() method to add user input as a parameter to the SQL query.
+    Completion 3: Use an ORM (Object-Relational Mapping) library like Entity Framework or NHibernate to handle the SQL queries, which provide a more secure way to handle user input.
+    --  
+    Prompt: "The following code is vulnerable to SQL injection. How can it be fixed?
+    cursor.execute("SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'")
+    result = cursor.fetchall()
 
-SqlCommand command = new SqlCommand(query, connection);
-SqlDataReader reader = command.ExecuteReader();
+    if len(result) > 0:
+        # login successful
+    else:
+        # login failed
+    "
+    Completion 1: Use prepared statements with parameterized queries to automatically escape any user input.
+    Completion 2: Use the psycopg2.extras.execute_values() method to pass a tuple of values to be used in the query instead of concatenating user input into the SQL query.
+    Completion 3: Use an ORM (Object-Relational Mapping) library like SQLAlchemy or Django ORM to handle the SQL queries, which provide a more secure way to handle user input.
+    -- 
+    Prompt: "The following code is vulnerable to SQL injection. How can it be fixed?
+    String query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'";
 
-if (reader.HasRows)
-{
-    // login successful
-}
-else
-{
-    // login failed
-}
-"
-Completion 1: Use prepared statements with parameterized queries to automatically escape any user input.
-Completion 2: Use the SqlCommand.Parameters.AddWithValue() method to add user input as a parameter to the SQL query.
-Completion 3: Use an ORM (Object-Relational Mapping) library like Entity Framework or NHibernate to handle the SQL queries, which provide a more secure way to handle user input.
---  
-Prompt: "The following code is vulnerable to SQL injection. How can it be fixed?
-cursor.execute("SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'")
-result = cursor.fetchall()
+    PreparedStatement statement = connection.prepareStatement(query);
+    ResultSet result = statement.executeQuery();
 
-if len(result) > 0:
-    # login successful
-else:
-    # login failed
-"
-Completion 1: Use prepared statements with parameterized queries to automatically escape any user input.
-Completion 2: Use the psycopg2.extras.execute_values() method to pass a tuple of values to be used in the query instead of concatenating user input into the SQL query.
-Completion 3: Use an ORM (Object-Relational Mapping) library like SQLAlchemy or Django ORM to handle the SQL queries, which provide a more secure way to handle user input.
---  
-Prompt: "The following code is vulnerable to SQL injection. How can it be fixed?
-query = "SELECT * FROM users WHERE username = '#{username}' AND password = '#{password}'"
-result = connection.exec(query)
-
-if result.ntuples > 0
-    # login successful
-else
-    # login failed
-end
-"
-Completion 1: Use prepared statements with parameterized queries to automatically escape any user input.
-Completion 2: Use the pg gem's escape() method to escape any special characters in user input before using it in an SQL query.
-Completion 3: Use an ORM (Object-Relational Mapping) library like ActiveRecord or Sequel to handle the SQL queries, which provide a more secure way to handle user input.
---
-Prompt: "The following code is vulnerable to SQL injection. How can it be fixed?
-String query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'";
-
-PreparedStatement statement = connection.prepareStatement(query);
-ResultSet result = statement.executeQuery();
-
-if (result.next()) {
-    // login successful
-} else {
-    // login failed
-}
-"
-Completion 1: Use prepared statements with parameterized queries to automatically escape any user input.
-Completion 2: Use the PreparedStatement.setString() method to set user input as a parameter to the SQL query instead of concatenating it into the query string.
-Completion 3: Use an ORM (Object-Relational Mapping) library like Hibernate or EclipseLink to handle the SQL queries, which provide a more secure way to handle user input.
---
-"The following code is vulnerable to SQL injection. How can it be fixed?
-    const username = "testuser";
-const password = "testpassword";
-const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
-
-connection.query(query, (error, result) => {
-    if (error) {
-        console.log(error);
+    if (result.next()) {
+        // login successful
     } else {
-        if (result.length > 0) {
-            console.log("login successful");
-        } else {
-            console.log("login failed");
-        }
+        // login failed
     }
-});
-"
-''',
-    max_tokens=400,
-    stop_sequences=["--"],
-    temperature=0.75)
+    "
+    Completion 1: Use prepared statements with parameterized queries to automatically escape any user input.
+    Completion 2: Use the PreparedStatement.setString() method to set user input as a parameter to the SQL query instead of concatenating it into the query string.
+    Completion 3: Use an ORM (Object-Relational Mapping) library like Hibernate or EclipseLink to handle the SQL queries, which provide a more secure way to handle user input.
+    --
+    "The following code is vulnerable to SQL injection. How can it be fixed? \n''' + code + '"'
 
-output = response.generations[0].text
-print(output)
+    print(prompt)
+    response = co.generate(
+        model='command-medium-nightly',
+        prompt = '''This program generates tips on how to fix code that is vulnerable to sql injection.
+
+    Prompt: "The following code is vulnerable to SQL injection. How can it be fixed?
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+    $result = mysqli_query($connection, $query);
+
+    if (mysqli_num_rows($result) > 0) {
+        // login successful
+    } else {
+        // login failed
+    }
+    "
+    Completion 1: Use prepared statements with parameterized queries to automatically escape any user input.
+    Completion 2: Use the mysqli_real_escape_string() function to escape any special characters in user input before using it in an SQL query.
+    Completion 3: Use an ORM (Object-Relational Mapping) library like Doctrine or Eloquent to handle the SQL queries, which provide a more secure way to handle user input.
+    --  
+    Prompt: "The following code is vulnerable to SQL injection. How can it be fixed?
+    const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
+
+    client.query(query, (err, res) => {
+        if (err) {
+            console.log(err.stack)
+        } else {
+            if (res.rows.length > 0) {
+                // login successful
+            } else {
+                // login failed
+            }
+        }
+    });
+    "
+    Completion 1: Use prepared statements with parameterized queries instead of concatenating user input into the SQL query.
+    Completion 2: Use a library like pg-escape to escape any special characters in user input before using it in an SQL query.
+    Completion 3: Use an ORM (Object-Relational Mapping) library like Sequelize or TypeORM to handle the SQL queries, which provide a more secure way to handle user input.
+    --  
+    Prompt: "The following code is vulnerable to SQL injection. How can it be fixed?
+    string query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'";
+
+    SqlCommand command = new SqlCommand(query, connection);
+    SqlDataReader reader = command.ExecuteReader();
+
+    if (reader.HasRows)
+    {
+        // login successful
+    }
+    else
+    {
+        // login failed
+    }
+    "
+    Completion 1: Use prepared statements with parameterized queries to automatically escape any user input.
+    Completion 2: Use the SqlCommand.Parameters.AddWithValue() method to add user input as a parameter to the SQL query.
+    Completion 3: Use an ORM (Object-Relational Mapping) library like Entity Framework or NHibernate to handle the SQL queries, which provide a more secure way to handle user input.
+    --  
+    Prompt: "The following code is vulnerable to SQL injection. How can it be fixed?
+    cursor.execute("SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'")
+    result = cursor.fetchall()
+
+    if len(result) > 0:
+        # login successful
+    else:
+        # login failed
+    "
+    Completion 1: Use prepared statements with parameterized queries to automatically escape any user input.
+    Completion 2: Use the psycopg2.extras.execute_values() method to pass a tuple of values to be used in the query instead of concatenating user input into the SQL query.
+    Completion 3: Use an ORM (Object-Relational Mapping) library like SQLAlchemy or Django ORM to handle the SQL queries, which provide a more secure way to handle user input.
+    -- 
+    Prompt: "The following code is vulnerable to SQL injection. How can it be fixed?
+    String query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'";
+
+    PreparedStatement statement = connection.prepareStatement(query);
+    ResultSet result = statement.executeQuery();
+
+    if (result.next()) {
+        // login successful
+    } else {
+        // login failed
+    }
+    "
+    Completion 1: Use prepared statements with parameterized queries to automatically escape any user input.
+    Completion 2: Use the PreparedStatement.setString() method to set user input as a parameter to the SQL query instead of concatenating it into the query string.
+    Completion 3: Use an ORM (Object-Relational Mapping) library like Hibernate or EclipseLink to handle the SQL queries, which provide a more secure way to handle user input.
+    --
+    "The following code is vulnerable to SQL injection. How can it be fixed? \n''' + code + '"',
+        max_tokens=400,
+        stop_sequences=["--"],
+        temperature=0.75)
+
+    return response.generations[0].text
+    

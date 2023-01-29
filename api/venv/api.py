ef04773ad8dcf5362c0400cc554b8957b982459b
colin_mccormack <colin.william.mccormack@gmail.com>
2023-01-28 23:30:40 -0500

    change api.py

--- api/venv/api.py
+++ api/venv/api.py
@@ -1,12 +1,13 @@
 import time
 from flask import Flask, request, render_template, jsonify
+# import nltk
 
 # some_file.py
 import sys
 # caution: path[0] is reserved for script path (or '' in REPL)
-sys.path.insert(1, '../ai')
+# sys.path.insert(1, '../ai')
 
-from model import classify
+# from model import classify
 
 app = Flask(__name__)
 
@@ -21,9 +22,12 @@ def form_example():
         except:
             return jsonify(result='ERROR : 400, bad request')
         # check code
-        if (not classify(text)):
-            return jsonify(result="WARNING : Your code is vulnerable to SQL Injection!")
-        else:
-            return jsonify(result="Your code is safe!")
+        print(text)
+        return jsonify(result="This is sent from the backend")
+
+        # if (not classify(text)):
+        #     return jsonify(result="WARNING : Your code is vulnerable to SQL Injection!")
+        # else:
+        #     return jsonify(result="Your code is safe!")
     # handle get request
     return jsonify(result='ERROR : 404, only POST req allowed or service not available')
